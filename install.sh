47d6db1828b386c872484f15fee2fce046ad325a
kev <kraven-daemon@pm.me>
2021-06-01 05:01:48 -0400

    added more files

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env sh
 
-fpath="$(realpath $0)"
+xdg(){
+	if [ -z "$1" ]; then
+		cat <<- MSG
+		XDG_CONFIG_HOME and/or XDG_DATA_HOME
+		are not set. check your env.
+
+		see https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+		for more information.
+		MSG
+	fi
+}
+
+xdg "$XDG_CONFIG_HOME"
+xdg "$XDG_DATA_HOME"
+
+FILES=$(ls -a)
+for line in $FILES; do
+	echo "$line"
+done
 
 
-echo "realpath : $fpath"

f40b404a0071864779152ebf3b49b6bb77587cfc
Kraven Daemon <kraven@pop-os.localdomain>
2021-07-23 14:18:42 -0400

    asdasd

--- install.sh
+++ install.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env sh
 
-xdg(){
-	if [ -z "$1" ]; then
-		cat <<- MSG
-		XDG_CONFIG_HOME and/or XDG_DATA_HOME
-		are not set. check your env.
-
-		see https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-		for more information.
-		MSG
-	fi
-}
-
-xdg "$XDG_CONFIG_HOME"
-xdg "$XDG_DATA_HOME"
 
 echo -e "In home"
 jq --raw-output ".home[]" ftree.json

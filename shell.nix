c2709f09ac1374a3b355af55966d949e9800d713
Kraven <kravendaemon@gmail.com>
2022-01-18 08:32:08 -0500

    (./bin/git_comm)

--- shell.nix
+++ shell.nix
@@ -1,10 +1,5 @@
-# This is me learning nix
-# Warning, too many comments are going to follow
-
 # shell.nix is read by nix-shell
-# this is to avoid running `nix-shell -p <program>` each time
 
-# now just run nix-shell to initialize the dev environnement
 { pkgs ? import <nixpkgs> {} # imports the set of functions from "nixpkgs"
 }:
 pkgs.mkShell {           # apparently this is an helper function runned each time you step in your shell env 

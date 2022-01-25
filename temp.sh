88d002e123cfaecd2ee768e5d4f2480aacb175ac
Kraven <kravendaemon@gmail.com>
2022-01-25 18:58:06 -0500

    Modified   =>  .aliases Modified   =>  .xinitrc Added      =>  .zshenv Added      =>  Xmodmap/Xmodmap Renamed    =>  .Xmodmap -> Xmodmap/xmodmap Added      =>  Xresources/Xresources Renamed    =>  .Xresources -> Xresources/xresources Added      =>  awesome/awesome Added      =>  awesome/rc.lua Added      =>  backgrounds/backgrounds Added      =>  conky/conky Added      =>  conky/conky.conf Added      =>  conky/conky.cpu Added      =>  fonts/fonts Deleted    =>  herbstluftwm/autostart Deleted    =>  herbstluftwm/panel.sh Deleted    =>  herbstluftwm/restartpanels.sh Added      =>  homepage/index.html Added      =>  list.bak Modified   =>  list.json Added      =>  temp.sh Added      =>  user-dirs.dirs Added      =>  user-dirs.locale

---
+++ temp.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env sh
+
+set -eu
+
+#!/usr/bin/env sh
+# depends: jq
+set -eu
+
+SRC="list.json"
+
+reset="\033[0m"
+## regular
+#red="\033[00;31m"
+#green="\033[00;32m"
+#yellow="\033[00;33m"
+#blue="\033[00;34m"
+#purple="\033[00;35m"
+#cyan="\033[00;36m"
+#lightgrey="\033[00;37m"
+## Bold
+bred="\033[01;31m"
+bgreen="\033[01;32m"
+byellow="\033[01;33m"
+#bblue="\033[01;34m"
+#bpurple="\033[01;35m"
+#bcyan="\033[01;36m"
+#white="\033[01;37m"
+
+# test wrapper, with colors
+# _test [flags] [file] [error message] [minimum char width for space padding] => return 0|1 to make it stoppable
+_test(){
+    flag=$1
+    file=$2
+    errmsg=$3
+    align=$4
+    retstatus=0
+    if test "$flag" "$file";then
+        printf "%-*s : %bOK%b\n" "${align}" "${file}" "${bgreen}" "${reset}"
+    else
+        printf "%-*s : %bFAIL%b\n%s : %s\n" "${align}" "${file}" "${bred}" "${reset}" "${file}" "${errmsg}"
+        printf "Exiting...\n"
+        retstatus=1
+    fi
+    return $retstatus
+}
+
+# return the caracter width of the longest word of a list
+_getlongest(){
+    long=0
+    next=0
+    for word in $1;do
+        next=$(printf "%s" "${word}" | wc -m)
+        if [ $long -lt "$next" ];then
+            long=$next
+        fi
+    done
+    echo "$long"
+}
+
+# iterate over list
+_linkable(){
+    list="$(jq -r ".$1[]" ${SRC})"
+    for file in $list
+    do
+		FILE="$(realpath "${file}")"
+		DEST="${2}/${file}"
+		printf "FILE : %s\nDESTINATION -> %s\n" "${FILE}" "${DEST}"
+		echo "LINKING ..."
+		if ln -s "${FILE}" "${DEST}"; then
+				printf "%bSucess%b linking %s to %s\n" "${bgreen}" "${reset}" "${FILE}" "${DEST}"
+		else
+				printf "Want to %boverride%b? y/n : " "${byellow}" "${reset}"
+				read  -r answer
+				case $answer in
+					y)
+						echo "removing ${DEST}"
+						rm "${DEST}"
+						ln -s "${FILE}" "${DEST}" 
+					;;
+					Y)
+						echo "removing ${DEST}"
+						rm "${DEST}"
+						ln -s "${FILE}" "${DEST}" 
+					;;
+					*)
+						;;
+				esac
+
+		fi
+    done
+}
+# ln -s "$(realpath "$link") to $2/$link"
+
+# iterate over keys
+_parse(){
+    iter=0
+    key=""
+    while :
+    do
+        key="$(jq -r "keys[$iter]" ${SRC})"
+        printf "\t%s :\t%s\n" "${iter}" "${key}"
+        case $key in
+            "config" )
+                echo "In $XDG_CONFIG_HOME"
+                _linkable "$key" "$XDG_CONFIG_HOME"
+                ;;
+            "home" )
+                echo "In $HOME/"
+                _linkable "$key" "$HOME"
+                ;;
+            "data" )
+                echo "In $XDG_DATA_HOME"
+                _linkable "$key" "$XDG_DATA_HOME"
+                ;;
+            "local" )
+                echo "In $HOME/.local"
+                _linkable "$key" "$HOME/.local"
+                ;;
+            "null" )
+                echo "List done!"
+                break
+                ;;
+        esac
+        iter="$(( iter + 1 ))"
+    done
+}
+
+_parse
